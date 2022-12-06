3336fe9a27cf1cce2fe87ace99e9d91ca13e74de
Jose <josemaria@hashed.io>
2022-12-06 17:51:42 -0600

    - Implemented BTC changes

--- src/ui/util/format-helper.js
+++ src/ui/util/format-helper.js
@@ -1,7 +1,9 @@
 module.exports = {
 
   formattedAmount: function(v){
-    let value = v == null || v.toFixed == null ? 0.00 : v.toFixed(2);
-    return parseFloat(value).toLocaleString('en-CA', { style: 'currency', currency: 'CAD' })
+    let value = v == null || v.toFixed == null ? 0.00 : v.toPrecision(8);
+    // return parseFloat(value).toLocaleString('en-CA', { style: 'currency', currency: 'CAD' })
+    const result = `${value} BTC`;
+    return result
   },
 };
